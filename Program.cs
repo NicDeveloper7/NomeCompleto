7eafb6cd20b1b58431757e85e5f7b47e86ce5c88
Nicholas <nicholasdev@zohomail.com>
2023-03-31 21:38:26 -0300

    Exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//Made in by Nicholasdev
+
+//Declaração de variável
+string nome, sobrenome;
+
+//Campo para o usuário preencher com seu nome e sobrenome
+Console.WriteLine("Digite seu Nome: ");
+nome = Console.ReadLine()!;
+Console.WriteLine("Digite seu Sobrenome: ");
+sobrenome = Console.ReadLine()!;
+
+//Exibe o nome de catálogo do usuário
+Console.Write("Seu nome de catálogo é: ");
+Console.Write($"{sobrenome} {nome}");
